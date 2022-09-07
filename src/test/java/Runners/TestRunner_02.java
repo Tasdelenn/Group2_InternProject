1015cef6e07edf38859ae31f02305690daa7b5fc
HaQaN <hakann.tasdelenn@gmail.com>
2022-09-07 14:26:28 +0300

    Adımlar 1.Runners klasöründe TestRunner_02.java class ı açıldı. 2.TestRunner_01 içeriği bu sınıfa kopyalandı 3.@CucumberOptions() içindeki features ={} 'ın içine bir tane daha features dosyası ("src/test/java/FeatureFiles/_TEC1_HR_Setup_PositionCategories.feature" eklendi. 4.Böylece eklenmiş olan tüm classlar (2 adet) sırayla çalıştı. 5.dryRun = False / True yorum u eklendi.

---
+++ src/test/java/Runners/TestRunner_02.java
@@ -0,0 +1,22 @@
+package Runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+
+        features = {"src/test/java/FeatureFiles/_01_Login.feature",
+                "src/test/java/FeatureFiles/_TEC1_HR_Setup_PositionCategories.feature"
+        },
+        glue = {"StepDefinitions"},
+
+        dryRun = false   // default u false yazmaya gerek yok. Ama true yaparsan
+                        // testi çalıştırmadan sadece adımların varlığına bakar...
+
+)
+public class TestRunner_02 extends AbstractTestNGCucumberTests {
+
+
+}
